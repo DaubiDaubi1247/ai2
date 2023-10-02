26fce2cc4eba77e190c2098607a45e89783b0133
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-10-02 12:58:55 +0300

    end dev algo

---
+++ my-app/src/common/artAlgorithmSolver/ArtAlgorithmSolver.ts
@@ -0,0 +1,107 @@
+
+export default class BinaryART1 {
+    private p: number;
+    private vigilance: number;
+    private beta: number;
+    public clusters : Claster[];
+    private addedInCluster : boolean;
+
+    constructor(inputSize: number, categorySize: number, vigilance: number) {
+        this.vigilance = vigilance;
+        this.beta = 1.0;
+        this.clusters = [];
+        this.p = 0.9;
+        this.addedInCluster = false;
+    }
+
+    train(input: number[][]): void {
+        this.clusters.push(new Claster(input[0]));
+        for (let index = 0; index < 10; index++) {
+            debugger
+            for (let i = 0; i < input.length; i++) {
+                let j = 0;
+                for (; j < this.clusters.length; j++) {
+                    if (this.clusters[j].isChildrenHasOwn(input[i])) {
+                        continue
+                    }
+    
+                    if ( this.isPassedAllTests(input[i], this.clusters[j].prototype)) {
+                        this.findVectorInAllClasterAndDel(this.clusters, input[i], j)
+                        this.clusters[j].addChild(input[i]);
+                        this.addedInCluster = true;
+                        break;
+                    }
+                }
+                if (j === this.clusters.length) {
+                    this.findVectorInAllClasterAndDel(this.clusters, input[i], -1)
+                    const newCluster = new Claster(input[i]);
+                    newCluster.addChild(input[i]);
+                    this.clusters.push(newCluster);
+                    
+                }
+            }
+        
+        }
+    }
+
+    private isPassedAllTests(vector : number[], vectorPrototype : number[]) {
+        return this.similarityTest(vector, vectorPrototype) && this.vigilanceTest(vector, vectorPrototype);
+    }
+
+    private vigilanceTest(vector : number[],  vectorPrototype: number[]): boolean {
+        const byteAndImportance = this.getImportance(vectorPrototype.map((el, i) => el & vector[i]))
+        
+        return byteAndImportance / this.getImportance(vector) >= this.vigilance
+    }
+
+// Функция для теста на схожесть
+    private similarityTest(vector : number[],  vectorPrototype: number[]): boolean {
+        const byteAndImportance = this.getImportance(vectorPrototype.map((el, i) => el & vector[i]))
+        const firstDrob = byteAndImportance / (this.beta + this.getImportance(vectorPrototype))
+
+        const secondDrob = (this.getImportance(vector) / (this.beta + vector.length))
+
+        return firstDrob > secondDrob ;
+    }
+
+    private getImportance(vector : number[]) : number {
+        return vector.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    }
+
+    private findVectorInAllClasterAndDel(clusters : Claster[], vectorE : number[], ignoreIndex : number) {
+        let delIndex = -1;
+
+        for (let i = 0; i < clusters.length; i++) {
+            if (i !== ignoreIndex) {
+                if ((delIndex = clusters[i].children.indexOf(vectorE)) !== -1) {
+                    clusters[i].children.splice(delIndex, 1);
+                    return
+                }
+            }
+        }
+    }
+
+}
+
+class Claster {
+    public prototype : number[]
+    public children : Array<number[]>;
+
+    constructor(prototype: number[]) {
+        this.prototype = [...prototype];
+        this.children = [];
+    }
+
+    public isChildrenHasOwn(vectorChild : number[]) : boolean {
+        return this.children.includes(vectorChild);
+    }
+
+    public addChild(vectorChild : number[]) {
+        this.children.push(vectorChild);
+        this.changePrototype(vectorChild);
+    }
+
+    public changePrototype(newChild : number[]) {
+        this.prototype = this.prototype.map((el, i) => el & newChild[i])
+    }
+}
--- my-app/src/components/main/Main.tsx
+++ my-app/src/components/main/Main.tsx
@@ -1,8 +1,5 @@
 import { FC, useState } from "react";
-import style from "./style/board.module.css"
-import MainInputs from "./mainInputs/mainInputs";
-import MainChart from "./mainCharts/MainCharts";
-import {QueenSolverStatistic} from "../../common/queenSolver/queenSolverStatistic";
+import BinaryART1 from "../../common/artAlgorithmSolver/ArtAlgorithmSolver";
 
 interface MainProps {
 
@@ -10,18 +7,43 @@ interface MainProps {
 
 const Main: FC<MainProps> = () => {
 
-    const [board, setboard] = useState<React.ReactNode[]>([])
-    const [chartIsVisible, setchartIsVisible] = useState(false)
-    const [queensSolverStatistic, setQueensSolverStatistic] = useState<QueenSolverStatistic | null>(null)
+    // Пример использования
+    const inputSize = 5; // Размер входных данных
+    const categorySize = 3; // Максимальное количество категорий
+    const vigilance = 0.9; // Параметр вигилиантности
+
+    const art1 = new BinaryART1(inputSize, categorySize, vigilance);
+
+// Обучение
+    const trainingData = [
+        [1,1,1,0],
+        [1,1,1,0],
+        [1,0,0,0],
+        [1,0,0,0]
+    ];
+
+
+
+    const onClickHandler = () => {
+        art1.train(trainingData)
+
+        let counter = 0;
+
+        for (const claster of art1.clusters) {
+            if (claster.children.length === 0) continue
+            console.log(`прототип`, claster.prototype)
+            console.log(`classter # ${counter}`, claster.children)
+            console.log()
+            counter++;
+        }
+    }
+
+// Вывод всех кластеров
+
 
     return (
         <div>
-            <MainInputs setBoard={setboard} setChartIsVisible={setchartIsVisible} setQueensSolverStatistic={setQueensSolverStatistic} />
-            <div className={style.chessboard}>
-                {board}
-            </div>
-
-            {chartIsVisible ? <MainChart queensSolverStatistic={queensSolverStatistic}/> : <></>}
+            <button onClick={onClickHandler}>Получить решение</button>
         </div>
     );
 }
