8c233257c2220f8a937541171441f1c4e338a3ce
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-10-02 17:47:54 +0300

    create interface for algo

--- my-app/src/components/main/OtputClusters/clusterView/ClusterView.tsx
+++ my-app/src/components/main/OtputClusters/clusterView/ClusterView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Claster} from "../../../../common/artAlgorithmSolver/ArtAlgorithmSolver";
+import randomColor from "randomcolor";
 
 interface ClusterProps {
     cluster : Claster,
@@ -7,6 +8,8 @@ interface ClusterProps {
 }
 
 const ClusterView : React.FC<ClusterProps>= ({cluster, counterClusters}) => {
+
+    const color = randomColor();
 
     const clusterItemsToJSX = (clusterItems : number[]) => clusterItems.map(el => <span >{el + " "}</span>)
 
@@ -17,11 +20,11 @@ const ClusterView : React.FC<ClusterProps>= ({cluster, counterClusters}) => {
             <h1>Кластер #{counterClusters}</h1>
             <div>
                 <h2>Прототип класстера</h2>
-                <span style={{border : "solid", padding : '5px'}}>{clusterItemsToJSX(cluster.prototype)}</span>
+                <span style={{border : "solid", padding : '5px', backgroundColor : color}}>{clusterItemsToJSX(cluster.prototype)}</span>
             </div>
             <div>
                 <h2>Вектора признаки входящие в этот класстер</h2>
-                <div style={{border : "solid"}}>{clusterChildren}</div>
+                <div style={{border : "solid", backgroundColor : color}}>{clusterChildren}</div>
             </div>
         </div>
     )
